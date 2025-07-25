0a69a8736e1269e109a9c0b1030f3bb4b1d7ffda
imtiazAhmd <imtiaz.ahmed@digital.justice.gov.uk>
2025-07-25 10:35:05 +0100

    Adds form validation rules and helpers

---
+++ types/form-validation.ts
@@ -0,0 +1,30 @@
+/**
+ * Common types and interfaces for form validation and error handling.
+ */
+
+export interface ValidationFields {
+  fullName: string;
+  existingFullName: string;
+}
+
+export interface InputError {
+  fieldName: string;
+  text: string;
+}
+
+export interface ErrorSummary {
+  text: string;
+  href?: string;
+}
+
+export interface ReturnValidation {
+  isInvalid: boolean;
+  errorSummary: ErrorSummary;
+  inputError?: InputError;
+}
+
+export interface ValidationResult  {
+  inputErrors: Record<string, string>;
+  errorSummaryList: ErrorSummary[];
+  formIsInvalid: boolean;
+};
