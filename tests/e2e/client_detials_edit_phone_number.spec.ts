4b8a2d9190f6d69f31a73b8162589106be7dfcb1
Masum Khan <masum.khan@digital.justice.gov.uk>
2025-08-01 16:26:04 +0100

    update test

--- tests/e2e/client_detials_edit_phone_number.spec.ts
+++ tests/e2e/client_detials_edit_phone_number.spec.ts
@@ -75,3 +75,41 @@ test('phoneNumber is not valid and correct validation errors display', async ({ 
   await expect(errorLinkSafeToCall).not.toBeVisible();
 });
 
+test('safeToCall & phoneNumber not changed and correct validation errors displayed', async ({ page }) => {
+  const saveButton = page.getByRole('button', { name: 'Save' });
+  const errorSummary = page.locator('.govuk-error-summary');
+  const errorLinkSafeToCall = page.locator('a[href="#safeToCall"]');
+  const errorLinkPhoneNumber = page.locator('a[href="#phoneNumber"]');
+  const phoneInput = page.locator('#phoneNumber');
+
+  // Navigate to the `/edit/phone-number`
+  await page.goto('/cases/PC-1922-1879/client-details/edit/phone-number');
+
+  // Find and click the save button
+  await expect(saveButton).toBeVisible();
+  await saveButton.click();
+
+  // Check GOV.UK error summary appears
+  await expect(errorSummary).toBeVisible();
+  await expect(errorSummary).toContainText('There is a problem');
+
+  // Submit form with blank phoneNumber
+  await page.locator('#phoneNumber').fill('');
+
+  // Find and click the save button
+  await expect(saveButton).toBeVisible();
+  await saveButton.click();
+
+  // Check GOV.UK error summary appears
+  await expect(errorSummary).toBeVisible();
+  await expect(errorSummary).toContainText('There is a problem');
+
+  // Check error summary links to problem field
+  await expect(errorLinkPhoneNumber).toBeVisible();
+  await expect(errorLinkPhoneNumber).toHaveText('Enter the client phone number');
+  await expect(phoneInput).toHaveClass(/govuk-input--error/);
+
+  // Check other error summary link not visible
+  await expect(errorLinkSafeToCall).not.toBeVisible();
+});
+
