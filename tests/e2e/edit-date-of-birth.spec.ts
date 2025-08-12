d417ce409c31c7c4bbe42c304fcad17ee5343275
Sandy Ryalls <sandy.ryalls@justice.gov.uk>
2025-08-12 09:13:06 +0100

    aligning e2e test with ui fixes

--- tests/e2e/edit-date-of-birth.spec.ts
+++ tests/e2e/edit-date-of-birth.spec.ts
@@ -4,7 +4,7 @@ test('viewing change date of birth form, to see the expected elements', async ({
   const dayInput = page.locator('#dateOfBirth-day');
   const monthInput = page.locator('#dateOfBirth-month');
   const yearInput = page.locator('#dateOfBirth-year');
-  const saveButton = page.getByRole('button', { name: /Save and continue/i });
+  const saveButton = page.getByRole('button', { name: /Save/i });
   
   // Navigate to the `/edit/date-of-birth`
   await page.goto('/cases/PC-1922-1879/client-details/edit/date-of-birth');
@@ -15,10 +15,4 @@ test('viewing change date of birth form, to see the expected elements', async ({
   await expect(monthInput).toBeVisible();
   await expect(yearInput).toBeVisible();
   await expect(saveButton).toBeVisible();
-  
-  // Check fieldset and legend for accessibility
-  const fieldset = page.locator('fieldset');
-  const legend = fieldset.locator('legend');
-  await expect(legend).toBeVisible();
-  await expect(legend).toContainText("What is the client's date of birth?");
 });
