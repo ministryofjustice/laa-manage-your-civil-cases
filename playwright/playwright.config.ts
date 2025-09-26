e366f41980f42df0ee4873822579a5e8e80d25df
Sandy Ryalls <sandy.ryalls@justice.gov.uk>
2025-09-26 13:51:10 +0100

    reorganising playwright folder structure

---
+++ playwright/playwright.config.ts
@@ -0,0 +1,55 @@
+import { defineConfig, devices } from '@playwright/test';
+
+const TRY_ZER0 = 0;
+const TRY_TWICE = 2;
+const WORKERS = 5;
+const TIMEOUT_MS = 60000;
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: Boolean(process.env.CI ?? false),
+  /* Retry on CI only */
+  retries: process.env.CI === 'true' ? TRY_TWICE : TRY_ZER0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI === 'true' ? WORKERS : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: 'http://localhost:3001',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: process.env.CI === 'true' ? 'on' : 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  webServer: {
+    command: 'yarn tsx scripts/test-server-with-msw.js',
+    url: 'http://127.0.0.1:3001',
+    reuseExistingServer: process.env.CI !== 'true',
+    stdout: 'pipe',
+    stderr: 'pipe',
+    timeout: TIMEOUT_MS,
+    env: {
+      NODE_ENV: 'test',
+      PORT: '3001',
+      API_URL: 'https://laa-civil-case-api-uat.cloud-platform.service.justice.gov.uk',
+      API_PREFIX: '/latest/mock'
+    }
+  },
+});
---
+++ playwright/tests/closed_client_detials.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '../fixtures/index.js';
+import { t, getClientDetailsUrlByStatus } from '../utils/index.js';
+
+test('client details selected from closed cases tab has correct page elements', async ({ page, i18nSetup }) => {
+  // Navigate to the client details
+  await page.goto(getClientDetailsUrlByStatus('closed'));
+
+  const closed_tag = page.getByText('Closed', { exact: true });
+  const reopen_case_button = page.getByRole('button', { name: t('pages.caseDetails.buttons.reopenCase') })
+  const legal_help_from_button = page.getByRole('button', { name: t('pages.caseDetails.buttons.generateLegalHelpForm') })
+
+  // expect to see the following elements
+  /* TODO: These expectations are not running and need to wait till MSW implementation.
+  *       Also need to change .toBeVisible to .toBeVisible() with parentheses.
+  */
+  await expect(closed_tag).toBeVisible;
+  await expect(reopen_case_button).toBeVisible;
+  await expect(legal_help_from_button).toBeVisible;
+});
+
+test('closed client details page should be accessible', {
+  tag: '@accessibility',
+}, async ({ page, checkAccessibility }) => {
+  await page.goto(getClientDetailsUrlByStatus('closed'));
+  await checkAccessibility();
+});
