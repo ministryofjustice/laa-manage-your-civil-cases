e76d34a609b016afd27a0a1bd9a98b1943f4039d
GemmaOwen1 <gemma.owen1@justice.gov.uk>
2026-01-26 13:19:33 +0000

    if applied, uses boolean to check menu button is visable

--- tests/playwright/utils/case-details-helper.ts
+++ tests/playwright/utils/case-details-helper.ts
@@ -1,5 +1,14 @@
 import { expect, Page } from '@playwright/test';
 
-export async function assertCaseDetailsHeaderPresent(page: Page, withoutMenuButtons: boolean) {
-  await expect(page.locator('.mcc-case-details-header')).toBeVisible();
+export async function assertCaseDetailsHeaderPresent(page: Page, withMenuButtons: boolean) {
+  const caseHeader = page.locator('.mcc-case-details-header');
+  await expect(caseHeader).toBeVisible();
+  if (withMenuButtons) {
+    assertMenuButtonVisible(page);
+  }
+}
+
+async function assertMenuButtonVisible(page: Page) {
+  const toggle = page.getByRole('button', { name: 'Change status' });
+  await expect(toggle).toBeVisible();
 }
