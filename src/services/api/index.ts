448aceebc44535c17afcb218930904354ab3ce47
GemmaOwen1 <gemma.owen1@justice.gov.uk>
2026-06-29 13:13:47 +0100

    if applied, adds financial eligibiliy partial with hardcoded answers

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -6,7 +6,7 @@
 // Export individual services
 // Import all services for creating the combined apiService object
 import { getCases, searchCases } from './resources/casesApiService.js';
-import { getClientDetails, updateClientDetails, getClientCaseLogs, getClientHistoryDetails, updateProviderNotes, changeCaseCategory } from './resources/clientDetailsApiService.js';
+import { getClientDetails, updateClientDetails, getClientCaseLogs, getClientHistoryDetails, updateProviderNotes, changeCaseCategory, getFinancialEligibility } from './resources/clientDetailsApiService.js';
 import {
   addThirdPartyContact,
   updateThirdPartyContact,
@@ -50,5 +50,6 @@ export const apiService = {
   getProviderChoices,
   getAllCategories,
   submitSplitCase, 
-  changeCaseCategory
+  changeCaseCategory,
+  getFinancialEligibility
 };
