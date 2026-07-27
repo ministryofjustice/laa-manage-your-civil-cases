f9f8a071bd2c082ffa8156fa69d840baa848b250
Alexander Tanner <alexander.justice@gov.uk>
2026-07-27 13:40:24 +0100

    feat: adds help page and footer link to the help page

--- routes/footerPages.ts
+++ routes/footerPages.ts
@@ -1,11 +1,15 @@
 import express from 'express';
+import { showHelpPage } from '#src/scripts/controllers/footerController.js'
 import { showCookiesPage } from '#src/scripts/controllers/cookiesController.js';
 
 // Create a new router for cookies
 const router = express.Router();
 
+/* GET help page. */
+router.get('/help', showHelpPage);
+
 /* GET cookies page. */
-router.get('/', showCookiesPage);
+router.get('/cookies', showCookiesPage);
 
 export default router
 
