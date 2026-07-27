b10ad4267cbe75bda8266a0d59960e728350d595
GemmaOwen1 <gemma.owen1@justice.gov.uk>
2026-07-27 14:39:00 +0100

    if applied, changes text based on if customer has partner

--- tests/playwright/tests/financial-eligibility.spec.ts
+++ tests/playwright/tests/financial-eligibility.spec.ts
@@ -116,13 +116,13 @@ test.describe('Finances tab', () => {
       'What is the current market value of the property?': '130000',
       'How much is left to pay on the mortgage?': '50000',
       'Is this your main property?': 'No',
-      'What percentage of the property do you and/or your partner own?': '100%'
+      'What percentage of the property do you own?': '100%'
     });
     await expectPropertyTableRows(page, '2nd property', {
       'What is the current market value of the property?': '120000',
       'How much is left to pay on the mortgage?': '60000',
       'Is this your main property?': 'Yes',
-      'What percentage of the property do you and/or your partner own?': '100%'
+      'What percentage of the property do you own?': '100%'
     });
 
     // Assert the correct data is displayed in the your savings table.
@@ -173,7 +173,7 @@ test.describe('Finances tab', () => {
       'What is the current market value of the property?': '150000',
       'How much is left to pay on the mortgage?': '60000',
       'Is this your main property?': 'Yes',
-      'What percentage of the property do you and/or your partner own?': '100%'
+      'What percentage of the property do you and/or your partner own?': '100%' 
     });
 
     // Assert the savings heading is visible.
@@ -801,5 +801,31 @@ test.describe('Conditional logic views', () => {
 
     // Disputed property row should be rendered
     await expect(page.getByText('Is the property disputed?')).toHaveCount(2);
+  });
+
+   test('when a customer has a partner the age over 60 text is different', async ({ page }) => {
+    const clientDetails = ClientDetailsPage.forCase(page, 'PC-1869-9154');
+    // Navigate to client details page
+    await clientDetails.navigate();
+    // Click the financial eligibility tab
+    await page.getByRole('link', { name: 'Financial eligibility' }).click();
+
+    // Assert the case details header is present
+    await assertCaseDetailsHeaderPresent(page, { withMenuButtons: false, expectedName: "Grace Baker", expectedCaseRef: "PC-1869-9154", dateReceived: "8 August 2025", badgeTexts: ['At risk of abuse', 'Third Party', 'Translation', 'BSL'] });
+    // Assert the URL has change to financial eligibility tab
+    await expect(page).toHaveURL(/financial-eligibility/);
+
+    // Assert the 'About you' header is visible
+    await expect(page.getByText('About you')).toBeVisible();
+
+    // Assert the correct data is displayed in the about you section
+    await expectCaptionTableRows(page, 'About you', {
+      'Are you aged 17 or under?': 'No',
+      'Do you have a partner?': 'Yes',
+      'Are you or your partner aged 60 or over?': 'No'
+    });
+   
+    // Assert the edit assessment button is visible.
+    await expect(page.getByRole('button', { name: 'Edit assessment' })).toBeVisible();
   });
 });
