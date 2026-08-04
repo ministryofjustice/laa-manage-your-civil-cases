a6af16a2b99084a6a42cba24c762359a648cb670
Alexander Tanner <alexander.justice@gov.uk>
2026-08-04 09:12:31 +0100

    Feat: updates accessibility link in the footer and adds accessibility info to accessibility page

--- routes/footerPages.ts
+++ routes/footerPages.ts
@@ -1,12 +1,15 @@
 import express from 'express';
-import { showHelpPage } from '#src/scripts/controllers/footerController.js';
+import { showHelpPage, showAccessibilityPage } from '#src/scripts/controllers/footerController.js';
 import { showCookiesPage } from '#src/scripts/controllers/cookiesController.js';
 
-// Create a new router for cookies
+// Create a new router for footer links pages
 const router = express.Router();
 
 /* GET help page. */
 router.get('/help', showHelpPage);
+
+/* GET accessibility page. */
+router.get('/accessibility', showAccessibilityPage);
 
 /* GET cookies page. */
 router.get('/cookies', showCookiesPage);
--- src/scripts/controllers/footerController.ts
+++ src/scripts/controllers/footerController.ts
@@ -1,10 +1,19 @@
 import type { Request, Response } from 'express';
 
 /**
- * Renders the cookies page.
+ * Renders the Help page.
  * @param {Request} req - Express request object
  * @param {Response} res - Express response object
  */
 export function showHelpPage(req: Request, res: Response): void {
     res.render('footer/help');
+}
+
+/**
+ * Renders the Accessibility page.
+ * @param {Request} req - Express request object
+ * @param {Response} res - Express response object
+ */
+export function showAccessibilityPage(req: Request, res: Response): void {
+    res.render('footer/accessibility');
 }
