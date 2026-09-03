405ed0627b24789f0f28a319f179693c662c5f94
Masum Khan <masum.khan@digital.justice.gov.uk>
2026-09-03 15:34:09 +0100

    If applied, this updates the folder structure

--- src/scripts/controllers/getLegalHelpFormController.ts
+++ src/scripts/controllers/getLegalHelpFormController.ts
@@ -47,7 +47,7 @@ export async function getLegalHelpForm(req: Request, res: Response, next: NextFu
       return res.redirect(`/cases/${caseReference}/client-details`);
     }
 
-    res.render('case_details/legal-help-form.njk', {
+    res.render('case_details/legal_help_form/legal-help-form.njk', {
       caseReference,
       provider,
       categoryItems,
